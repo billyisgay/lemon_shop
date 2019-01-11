aee79819a743d651a3a26fadaa31638a9be5aa66
James Round <13RoundJ@chase.worcs.sch.uk>
2019-01-11 12:55:11 +0000

    working at last

--- Program.cs
+++ Program.cs
@@ -6,16 +6,66 @@ using System.Threading.Tasks;
 
 namespace lemon_shop
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            String username;
+            String password;
+            String[,] accnts = { { "cads123", "dadada" }, { "bildo", "1234" }, { "delossantos", "gagaga" } };
+            int row;
+            bool isValideUser = false;
+            for (int x = 3; x >= 1; x--)
+            {
+                Console.WriteLine("You have " + x + " attempt/s.");
+                Console.Write("Enter Username>> ");
+                username = Console.ReadLine();
+                Console.Write("Enter Password>> ");
+                password = Console.ReadLine();
+                for (row = 0; row < 3; row++)
+                {
+                    if (username.Equals(accnts[row, 0]) && password.Equals(accnts[row, 1]))
+                    {
+                        Console.WriteLine("Welcome " + accnts[row, 0] + "!");
+                        isValideUser = true;
+                        break;
+                    }
+                }
+                if (!isValideUser)
+                {
+                    Console.WriteLine("Invalid Input.");
+                    if (x != 1)
+                    {
+                        Console.WriteLine("Please Try Again.");
+                        Console.Write("\n");
+                    }
+                    else if (x.Equals(1))
+                    {
+                        Console.Write("Goodbye!");
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            {
+                Console.WriteLine("login correct");
+                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("welcome to the shop");
+                Random rnd = new Random();
+                int till_float = rnd.Next(0, 11);
+                Console.WriteLine(till_float);
+                Console.WriteLine("what do you want to do?");
+                Console.WriteLine("check till works or 1");
+                Console.WriteLine("check float or 2");
+                Console.WriteLine("wait for customers or 3");
+                Console.ReadLine();
+                //// case switch needed for each of the options above to be done in CS
+            }
         }
     }
 }
+            
